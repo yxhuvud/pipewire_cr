ccdf80f9212b291b0857427cad9614597c2a0366
expeehaa <expeehaa@outlook.com>
2026-02-12 00:45:14 +0100

    Replace explicit listener classes with generation by macro.

---
+++ src/pipewire/event_listener.cr
@@ -0,0 +1,59 @@
+module Pipewire
+  module EventListener
+    macro event_listener(callback)
+      {% callback_name = callback.var %}
+      {% callback_type = callback.type %}
+      {% type_name = @type.name.split("::").last.id %}
+      {% events_type = parse_type("Pipewire::LibPipewire::#{type_name}Events").resolve %}
+
+      def on_{{ callback_name }}(&callback : {{ callback_type }})
+        EventListener{{ callback_name.camelcase }}.new(self, callback).tap do |event_listener|
+          self.event_listeners_{{ callback_name }} << event_listener
+        end
+      end
+
+      protected getter event_listeners_{{ callback_name }} = [] of EventListener{{ callback_name.camelcase }}
+
+      class EventListener{{ callback_name.camelcase }}
+        def initialize(@host : {{ @type }}, callback : {{ callback_type }})
+          @hook = ::Pipewire::LibSPA::Hook.new
+          @box = ::Box.box(callback)
+          @events = {{ events_type }}.new(
+            version: {{ @type }}::EVENT_LISTENER_VERSION,
+            {{ callback_name }}: ->(data : Void*,
+              {% for unresolved_type, i in callback_type.inputs %}
+                {% t = unresolved_type.resolve %}
+                arg{{ i }} : {% if t.name.starts_with?("Pipewire::LibPipewire::") || t.name.starts_with?("Pipewire::LibSPA::") %}
+                    {{ t }}*,
+                  {% elsif t <= parse_type("Pipewire::Base").resolve %}
+                    {{ t.ancestors.find { |a| a.name(generic_args: false) == "Pipewire::Base" }.type_vars.first }}*,
+                  {% elsif t.resolve == String %}
+                    LibC::Char*,
+                  {% else %}
+                    {{ t }},
+                  {% end %}
+              {% end %}
+            ) do
+            cb = Box(typeof(callback)).unbox(data)
+            cb.call(
+              {% for unresolved_type, i in callback_type.inputs %}
+                {% t = unresolved_type.resolve %}
+                {% if t.name.starts_with?("Pipewire::LibPipewire::") || t.name.starts_with?("Pipewire::LibSPA::") %}
+                  arg{{ i }}.value,
+                {% elsif t <= parse_type("Pipewire::Base").resolve %}
+                  {{ t }}.new(arg{{ i }}),
+                {% elsif t.resolve == String %}
+                  String.new(arg{{ i }}),
+                {% else %}
+                  arg{{ i }},
+                {% end %}
+              {% end %}
+            )
+          end)
+
+          LibPipewire.pw_{{ type_name.downcase }}_add_listener(@host, pointerof(@hook), pointerof(@events), @box)
+        end
+      end
+    end
+  end
+end
--- src/pipewire/registry.cr
+++ src/pipewire/registry.cr
@@ -1,49 +1,15 @@
 require "../lib/lib_pipewire"
 require "./base"
+require "./event_listener"
 
 module Pipewire
   class Registry < Base(LibPipewire::Registry)
-    class EventListenerGlobal
-      def initialize(@registry : Registry, callback : UInt32, UInt32, String, UInt32, Pipewire::LibSPA::Dict -> Void)
-        @hook = LibSPA::Hook.new
-        @box = Box.box(callback)
-        @events = LibPipewire::RegistryEvents.new(version: LibPipewire::VERSION_REGISTRY_EVENTS, global: ->(data : Void*, id : UInt32, permissions : UInt32, item_type : LibC::Char*, version : UInt32, props : Pipewire::LibSPA::Dict*) do
-          cb = Box(typeof(callback)).unbox(data)
-          cb.call(id, permissions, String.new(item_type), version, props.value)
-        end)
+    include EventListener
 
-        LibPipewire.pw_registry_add_listener(@registry, pointerof(@hook), pointerof(@events), @box)
-      end
-    end
+    EVENT_LISTENER_VERSION = LibPipewire::VERSION_REGISTRY_EVENTS
 
-    protected getter event_listeners_global = [] of EventListenerGlobal
-
-    def on_global(&callback : UInt32, UInt32, String, UInt32, Pipewire::LibSPA::Dict -> Void)
-      self.event_listeners_global << EventListenerGlobal.new(self, callback)
-    end
-
-    class EventListenerGlobalRemove
-      def initialize(@registry : Registry, callback : UInt32 -> Void)
-        @hook = LibSPA::Hook.new
-        @box = Box.box(callback)
-        @events = LibPipewire::RegistryEvents.new(version: LibPipewire::VERSION_REGISTRY_EVENTS, global_remove: ->(data : Void*, id : UInt32) do
-          cb = Box(typeof(callback)).unbox(data)
-          cb.call(id)
-        end)
-
-        LibPipewire.pw_registry_add_listener(@registry, pointerof(@hook), pointerof(@events), @box)
-      end
-    end
-
-    protected getter event_listeners_global_remove = [] of EventListenerGlobalRemove
-
-    def on_global_remove(&callback : UInt32 -> Void)
-      self.event_listeners_global_remove << EventListenerGlobalRemove.new(self, callback)
-    end
-
-    def to_unsafe
-      @registry
-    end
+    event_listener global : UInt32, UInt32, String, UInt32, Pipewire::LibSPA::Dict -> Void
+    event_listener global_remove : UInt32 -> Void
 
     def finalize
       LibPipewire.pw_proxy_destroy(self.to_unsafe.as(Pointer(LibPipewire::Proxy)))
