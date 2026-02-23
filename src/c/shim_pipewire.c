f7ec44412bd14dba772fefe986228620e128f9aa
expeehaa <expeehaa@outlook.com>
2026-02-24 00:01:48 +0100

    Format C code to be consistent.

--- src/c/shim_pipewire.c
+++ src/c/shim_pipewire.c
@@ -4,25 +4,20 @@ const char* pw_get_headers_version_shim() {
   return pw_get_headers_version();
 }
 
-void
-pw_loop_enter_shim(struct pw_loop *loop) {
+void pw_loop_enter_shim(struct pw_loop *loop) {
   pw_loop_enter(loop);
   return;
 }
 
-void
-pw_loop_leave_shim(struct pw_loop * loop) {
+void pw_loop_leave_shim(struct pw_loop * loop) {
   pw_loop_leave(loop);
   return;
 }
 
-
-int
-pw_loop_iterate_shim(struct pw_loop * loop, int timeout) {
+int pw_loop_iterate_shim(struct pw_loop * loop, int timeout) {
   return pw_loop_iterate(loop, timeout);
 }
 
-int
-pw_loop_get_fd_shim(struct pw_loop * loop) {
+int pw_loop_get_fd_shim(struct pw_loop * loop) {
   return pw_loop_get_fd(loop);
 }
