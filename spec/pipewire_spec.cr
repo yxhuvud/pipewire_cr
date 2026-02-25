4a2abd8af146f7e4f14ea59ff125b8cc190622ae
expeehaa <expeehaa@outlook.com>
2026-02-25 23:44:04 +0100

    Remove tests that are or were intended to be based on tutorials without an exit condition.

--- spec/pipewire_spec.cr
+++ spec/pipewire_spec.cr
@@ -1,94 +1,11 @@
 require "./spec_helper"
 
 describe Pipewire do
-  # TODO: Write tests
-  it "can enumerate objects" do
-    # https://docs.pipewire.org/page_tutorial2.html
-    Pipewire.init "hello"
-    main_loop = Pipewire::MainLoop.new
-    context = main_loop.create_context
-    core = context.connect
-    #     registry = pw_core_get_registry(core, PW_VERSION_REGISTRY,
-    #                 0 /* user_data size */);
-
-    # spa_zero(registry_listener);
-    # pw_registry_add_listener(registry, &registry_listener,
-    #                                &registry_events, NULL);
-
-    # pw_main_loop_run(loop);
-
-    # pw_proxy_destroy((struct pw_proxy*)registry);
-    # pw_core_disconnect(core);
-    # pw_context_destroy(context);
-    # pw_main_loop_destroy(loop);
-
-    # return 0;
-  end
-
   it "can do a roundtrip" do
     # https://docs.pipewire.org/page_tutorial3.html
     Pipewire.init "hello"
     main_loop = Pipewire::MainLoop.new
     context = main_loop.create_context
     core = context.connect
-  end
-
-  it "can play a jig" do
-    # https://docs.pipewire.org/page_tutorial4.html
-    # TODO: Extract system defaults
-    rate = 44100
-    channels = 2
-    volume = 0.7
-
-    Pipewire.init "hello"
-    main_loop = Pipewire::MainLoop.new
-    context = main_loop.create_context
-    core = context.connect
-
-    buffer = Slice(UInt8).new(1024)
-    pod_builder = Pipewire::SPA::PodBuilder.new(buffer)
-
-    stream = core.create_stream(
-      "audio-src",
-      {
-        Pipewire::PropertyKey::MEDIA_TYPE     => "Audio",
-        Pipewire::PropertyKey::MEDIA_CATEGORY => "Playback",
-        Pipewire::PropertyKey::MEDIA_ROLE     => "Music",
-      }
-    )
-
-    stream.on_process do
-      p :hi
-    end
-
-    positions = StaticArray(UInt32, Pipewire::LibSPA::MAX_CHANNELS).new { 0u32 }
-    info = Pipewire::LibSPA::AudioInfoRaw.new(
-      format: Pipewire::AudioFormat::S16,
-      rate: rate,
-      channels: channels,
-      position: positions
-    )
-    pod = Pipewire::LibSPA.spa_format_audio_raw_build(
-      pod_builder,
-      Pipewire::LibSPA::ParamType::EnumFormat,
-      pointerof(info),
-    )
-    params = [pod]
-
-    stream.connect(
-      params: params,
-      direction: Pipewire::LibPipewire::Direction::Output,
-      flags: Pipewire::Stream::Flag::Autoconnect |
-             Pipewire::Stream::Flag::MapBuffers
-    )
-    main_loop.process_all
-  end
-
-  it "can capture video frames" do
-    # https://docs.pipewire.org/page_tutorial5.html
-  end
-
-  it "can bind to objects so that events are received" do
-    # https://docs.pipewire.org/page_tutorial6.html
   end
 end
