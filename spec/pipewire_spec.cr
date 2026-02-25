49ce131440ef73ab65f123728c9cb961c285eef3
expeehaa <expeehaa@outlook.com>
2026-02-25 23:52:57 +0100

    Write a test based on tutorial 3.

--- spec/pipewire_spec.cr
+++ spec/pipewire_spec.cr
@@ -1,11 +1,29 @@
 require "./spec_helper"
 
 describe Pipewire do
-  it "can do a roundtrip" do
-    # https://docs.pipewire.org/page_tutorial3.html
-    Pipewire.init "hello"
+  # based on https://docs.pipewire.org/page_tutorial3.html
+  it "can enumerate registry objects" do
     main_loop = Pipewire::MainLoop.new
     context = main_loop.create_context
     core = context.connect
+    registry = core.registry
+
+    object_ids = Set(UInt32).new
+
+    registry.on_global do |id, permissions, item_type, version, properties|
+      object_ids << id
+    end
+
+    pending = core.sync(0)
+
+    core.on_done do |id, seq|
+      if id == Pipewire::LibPipewire::ID_CORE && seq == pending
+        main_loop.quit
+      end
+    end
+
+    main_loop.run.should eq 0
+
+    object_ids.size.should be > 0
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,4 @@
 require "spec"
 require "../src/pipewire"
+
+Pipewire.init("pipewire_spec")
