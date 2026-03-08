ff4258560a716aa604ca0d2f192dfe0b1cbb898f
expeehaa <expeehaa@outlook.com>
2026-03-08 21:58:38 +0100

    Add script and Dockerfile to run tests in a container.

---
+++ bin/test_container.cr
@@ -0,0 +1,26 @@
+#!/usr/bin/env crystal
+
+require "process"
+
+TEST_IMAGE_NAME = "crystal_pipewire_test"
+
+container_runtime = Process.find_executable("podman")
+container_runtime = Process.find_executable("docker") unless container_runtime
+
+unless container_runtime
+  STDERR.puts %q{Could not find executables "podman" or "docker".}
+  exit 1
+end
+
+root_dir = File.expand_path(File.join(__DIR__, ".."))
+
+build_result = Process.run(container_runtime, ["build", "-f", File.join(root_dir, "spec", "Dockerfile"), "-t", TEST_IMAGE_NAME, root_dir], output: Process::Redirect::Inherit, error: Process::Redirect::Inherit)
+exit build_result.exit_code unless build_result.success?
+
+run_result = Process.run(container_runtime, ["run", "--rm", "--tty", "--volume", "#{root_dir}:/app:O", TEST_IMAGE_NAME, "/bin/bash", "-c", <<-COMMAND], output: Process::Redirect::Inherit, error: Process::Redirect::Inherit)
+  dbus-daemon --system --fork
+  pipewire &
+  crystal spec --stats --progress -Dspec_in_container
+  COMMAND
+
+exit run_result.exit_code
